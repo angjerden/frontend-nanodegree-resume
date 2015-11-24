0168e8d61a9ce0d36b5872177de11148908575a9
Anders Gjerde <angjerden@gmail.com>
2015-11-24 18:35:12 +0100

    Added json objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,67 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name" : "Fry",
+    "role" : "Pizza guy",
+    "contacts" : [{
+        "mobile" : "123456",
+        "email" : "fry@planetexpress.com",
+        "location" : "New New York",
+        "twitter" : "@fry",
+        "github" : "fry"
+    }],
+    "welcomeMessage" : "Shut up and take my money!",
+    "image" : "images/fry.jpg",
+    "skills" : ["Walking","Sneezing"]
+};
+
+var education = {
+    "schools" : [
+        {
+            "name" : "Slacker University",
+            "location" : "New New York",
+            "degree" : "Bachelor",
+            "majors" : "Nosepicking",
+            "dates" : "2003-2005",
+            "url" : "www.slacker.com"
+        },
+        {
+            "name" : "Nogood College",
+            "location" : "Upstate New York",
+            "degree" : "Master",
+            "majors" : "Traveling",
+            "dates" : "2005 - 2006",
+            "url" : "www.nogood.com"
+        }],
+    "online courses" : [{
+        "title" : "Sleeping",
+        "school" : "No",
+        "dates" : "1970-1980",
+        "url" : "www.dontwanna.com"
+    }]
+};
+
+var work = {
+    "jobs" : [
+        {
+            "employer" : "My boss",
+            "title" : "Boss",
+            "dates" : "1990-3000",
+            "location" : "New New York",
+            "description" : "Nothing much."
+        }
+    ]
+};
+
+var projects = {
+    "project" : [{
+        "title" : "Watching grass grow",
+        "dates" : "2014",
+        "description" : "The fascinating study of why we have to mow lawns",
+        "images" : ["images/1.jpg","images/2.jpg"]
+    },
+        {
+            "title" : "Whatever",
+            "dates" : "2014",
+            "description" : "Dunno",
+            "images" : ["images/1.jpg"]
+        }]
+};
